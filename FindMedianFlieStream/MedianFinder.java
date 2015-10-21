ab449084f7db690e6eb9ee559e3e9dc3d6ac07a2
Alan <hcallen.wang@gmail.com>
2015-10-21 17:04:49 -0400

    10/21

---
+++ FindMedianFlieStream/MedianFinder.java
@@ -0,0 +1,127 @@
+import java.util.PriorityQueue;
+import java.util.Comparator;
+import java.util.Collections;
+
+public class MedianFinder{
+    private static PriorityQueue<Integer> minHeap;
+    private static PriorityQueue<Integer> maxHeap;
+    private static PriorityQueue<Integer> printminHeap;
+    private static PriorityQueue<Integer> printmaxHeap;
+
+    public MedianFinder(){
+    	this.minHeap=new PriorityQueue<Integer>();
+    	this.maxHeap=new PriorityQueue<Integer>();
+    	this.printminHeap=minHeap;
+    	this.printmaxHeap=maxHeap;
+    }
+
+	// Adds a number into the data structure.
+    public static void addNum(int num) {
+    	if(maxHeap.size()==0)
+    		maxHeap.offer(num);
+    	else{
+    		if(minHeap.size()==0){
+    			maxHeap.offer(num);
+    			minHeap.offer(maxHeap.poll());
+    		}
+    		else{
+    			if(num>minHeap.peek()){
+    				minHeap.offer(num);
+    				maxHeap.offer(minHeap.poll());
+    			}
+    			else{
+    				maxHeap.offer(num);
+    			}
+    			if(maxHeap.size()-minHeap.size()>1){
+    				minHeap.offer(maxHeap.poll());
+    			}
+    		}
+    	}
+    }
+
+    // Returns the median of current data stream
+    public static double findMedian() {
+    	maxHeap=reversePQ();
+        if(minHeap.size()==maxHeap.size())	//The size of stream is even
+        	return ((double)minHeap.peek()+(double)maxHeap.peek())/2.0;
+        else	//The size of stream is odd
+        	return (double)(minHeap.size()>maxHeap.size()?minHeap.peek():maxHeap.peek());
+    }
+
+    private static PriorityQueue<Integer> reversePQ(){
+    	PriorityQueue<Integer> reversed=new PriorityQueue<Integer>(maxHeap.size(), Collections.reverseOrder());
+    	reversed.addAll(maxHeap);
+    	return reversed;
+    }
+
+    // private static void addNumToHeap(LinkedList<Integer> list, int num, boolean isReverse){
+    // 	// int i=(isReverse)?list.size()-1:0;
+    // 	// if(isReverse){
+    // 	// 	while(i>=0 && num>=list.get(i))
+    // 	// 		i--;
+    // 	// 	if(i==list.size()-1 && i!=0) list.add(i,num);
+    // 	// 	else list.add(i+1,num);
+    // 	// }
+    // 	// else{
+    // 	// 	while(i<list.size() && num<=list.get(i))
+    // 	// 		i++;
+    // 	// 	if(i==0) list.add(i+1,num);
+    // 	// 	else list.add(i-1,num);
+    // 	// }
+    // 	if(isReverse){
+    // 		if(num>=list.getFirst())
+    // 			list.addFirst(num);
+    // 		else list.addLast(num);
+    // 	}
+    // 	else{
+    // 		if(num<=list.getFirst())
+    // 			list.addFirst(num);
+    // 		else list.addLast(num);
+    // 	}
+    // }
+
+    public static void printHeap(){
+    	System.out.print("Min heap is: ");
+    	while(printminHeap.size()>0)
+    		System.out.print(printminHeap.poll()+" ");
+    	System.out.println();
+    	System.out.print("Max heap is: ");
+    	while(printmaxHeap.size()>0)
+    		System.out.print(printmaxHeap.poll()+" ");
+    }
+
+    public static void main(String[] args) {
+    	MedianFinder mf=new MedianFinder();
+    	mf.addNum(6);
+    	System.out.println("median="+mf.findMedian());
+    	mf.addNum(10);
+    	System.out.println("median="+mf.findMedian());
+    	mf.addNum(2);
+    	//mf.printHeap();
+    	System.out.println("median="+mf.findMedian());
+    	mf.addNum(6);
+    	//mf.printHeap();
+    	System.out.println("median="+mf.findMedian());
+    	mf.addNum(5);
+    	//mf.printHeap();
+    	System.out.println("median="+mf.findMedian());
+    	mf.addNum(0);
+    	//mf.printHeap();
+    	System.out.println("median="+mf.findMedian());
+    	mf.addNum(6);
+    	System.out.println("median="+mf.findMedian());
+    	mf.addNum(3);
+    	System.out.println("median="+mf.findMedian());
+    	mf.addNum(1);
+    	System.out.println("median="+mf.findMedian());
+    	//mf.printHeap();
+    	mf.addNum(0);
+    	System.out.println("median="+mf.findMedian());
+    	//mf.addNum(0);
+    	//System.out.println("median="+mf.findMedian());
+
+    	//mf.printHeap();
+    	//System.out.println("median="+mf.findMedian());
+    	//mf.printHeap();
+    }
+}
